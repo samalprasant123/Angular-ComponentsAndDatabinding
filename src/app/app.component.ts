ede3920d7683f140bea410c8ac1a3b32200b3225
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-04-14 22:27:33 -0400

    Binding to Custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
 
   serverElements = [{type: 'server', name: 'TestServer', content: 'This is a test content.'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
 }
